2c473df28e1b9b1c8865d43beb94202292fd9832
James Davison <jamesdavison@jamess-mbp.home>
2023-05-22 11:42:20 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+import openai
+import streamlit as st
+from text_summarizer.functions import summarize
+
+try: 
+    # get api key
+    openai.api_key = os.getenv('OPENAI_KEY')
+
+    # initialize state variable 
+    if "summary" not in st.session_state:
+        st.session_state["summary"] = ""
+
+    st.title("Text Summarizer")
+
+    input_text = st.text_area(label='Enter full text:', value="", height=250)
+
+    st.button(
+        "Submit",
+        on_click=summarize,
+        kwargs={"prompt": input_text},
+    )
+
+    # configure text area to populate with current state of summary
+    output_text = st.text_area(label='Summarized text:', value=st.session_state["summary"], height=250)
+except:
+    st.write('There was an error')
